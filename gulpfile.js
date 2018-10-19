39e5f6c6d7eeaa975b3b92ec64ef681f3ae47fe9
Denis <disamapm@gmail.com>
2018-10-19 16:31:58 +0300

    Добавляет команду build

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp      = require('gulp'),
     browserSync = require('browser-sync'),
     cssnano     = require('gulp-cssnano'),
     rename      = require('gulp-rename'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    del         = require('del');
 
 gulp.task('sass', function(){ 
     return gulp.src('app/sass/**/*.scss') 
@@ -33,4 +34,31 @@ gulp.task('watch', ['browser-sync', 'css-libs', 'sass'], function() {
   gulp.watch('app/sass/**/*.scss', ['sass']); 
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist'); 
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+gulp.task('build', ['clean', 'sass'], function() {
+
+    var buildCss = gulp.src([ 
+        'app/css/style.css',
+        'app/css/style.min.css'
+        ])
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') 
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') 
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') 
+    .pipe(gulp.dest('dist'));
+
 });
