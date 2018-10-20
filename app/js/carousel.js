481175f59064d1c759f0fb558e17d0411b6f8952
Denis <disamapm@gmail.com>
2018-10-20 10:23:09 +0300

    Делает карусель по кнопкам вперед/назад

--- app/js/carousel.js
+++ app/js/carousel.js
@@ -1,4 +1,4 @@
-var numberOfSlides = 4;
+var numberOfSlides = 3;
 var slideWidth = 954;
 
 var carousel = document.querySelector(".carousel__wrapper");
@@ -6,4 +6,20 @@ var paginator = document.querySelector(".slider-paginator");
 var prev = paginator.querySelector(".slider-paginator__button-prev");
 var next = paginator.querySelector(".slider-paginator__button-next");
 
-prev.onclick
+next.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  
+  var prevPosition = parseInt(carousel.style.transform.slice(11)) || 0;
+  if (prevPosition != (-(slideWidth * numberOfSlides))) {
+    carousel.style.transform = "TranslateX("+ (prevPosition - slideWidth) +"px)"
+  } else {}
+})
+
+prev.addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+  var prevPosition = parseInt(carousel.style.transform.slice(11)) || 0;
+  if (prevPosition != 0) {
+    carousel.style.transform = "TranslateX("+ (prevPosition + slideWidth) +"px)";
+  } else {}
+})
