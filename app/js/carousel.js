63964d7b74ff74a6c694d9e600a327d8bb895a76
Denis <disamapm@gmail.com>
2018-10-20 09:39:39 +0300

    Переменные для карусели

---
+++ app/js/carousel.js
@@ -0,0 +1,9 @@
+var numberOfSlides = 4;
+var slideWidth = 954;
+
+var carousel = document.querySelector(".carousel__wrapper");
+var paginator = document.querySelector(".slider-paginator");
+var prev = paginator.querySelector(".slider-paginator__button-prev");
+var next = paginator.querySelector(".slider-paginator__button-next");
+
+prev.onclick
